cc0e35c90ca98f3e34a11b352159ea7782e23915
suciartiara <artiaras22d@student.unhas.ac.id>
2024-12-15 18:00:40 +0800

    add dummy data

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['name', 'description', 'price'];
 
     // Metode untuk mendapatkan produk dengan caching Redis
@@ -46,4 +49,5 @@ class Product extends Model
         $this->clearCache();
         return parent::delete();
     }
+
 }
